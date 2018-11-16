88112df5fad5e05b865c6f965c766c6255dc5763
veselinn16 <veselinn16@gmail.com>
2018-11-16 21:31:08 +0200

    Render submit answer button :wrench:

--- src/Question.js
+++ src/Question.js
@@ -4,16 +4,29 @@ class Question extends Component {
     render() {
         const {question} = this.props // unpack question from props object
         return(
-            <div className="contain">
-                <div className="question">
+            <div className="container">
+                {/* <div className="question">
                     <h1 className="question">{question.question}</h1>
-                    {question.answersArray ? question.answersArray.map(answer => {
+                    {question.answersArray ? question.answersArray.map((answer, i) => {
                         return (
                         <label>
-                            <input className="ans" type="checkbox" key={question.answersArray.indexOf(answer)} value={answer}/>
+                            <input className="ans" type="checkbox" key={i} value={answer}/>
                         {answer}</label>
                         )
                     }) : <h1>Please Enter a Question!</h1>}
+                </div> */}
+                <div className="question-container">
+                    <h1 className="question">Which year was Bulgaria established?</h1>
+                    <div className="answers">
+                        {[682, 681, 732, 1362].map((answer, i) => {
+                            return (
+                            <label>
+                                <input className="ans" type="radio" key={i} name="answer"/>
+                            {answer}</label>
+                            )
+                        })}
+                    </div>
+                    <button className="answer-btn">Submit Answer</button>
                 </div>
             </div>
         )
