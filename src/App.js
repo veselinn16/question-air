27d7446845911ed85204d1d87607ebb7bb938214
veselinn16 <veselinn16@gmail.com>
2018-11-16 11:34:14 +0200

    Add function for getting data from form :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+
+import Form from './Form';
+import Question from './Question';
 
 class App extends Component {
+  state = {
+    question: ''
+  }
+
+  getData = e => {
+    // prevent default behabior of submit button
+    e.preventDefault();
+
+    // extract text from HTML nodes
+    let question = document.querySelector('.question').value;
+    let answer = document.querySelector('.correct').value;
+    let answerArr1 = [].slice.call(document.querySelectorAll('.answer')); // convert nodelist to array
+    let answerArr = answerArr1.map(ans => ans.value); // extract text
+    
+    let questionObj = {
+        question,
+        answer,
+        answerArr            
+    }
+
+    this.setState({question: questionObj});
+    
+    console.log(`Question: ${questionObj.question}`);
+    console.log(`Correct: ${questionObj.answer}`);
+    console.log(`Answer array: ${questionObj.answerArr[0]}`);
+  }
+
   render() {
     return (
       <div className="App">
-        <form className="form">
-          <input className="question" type="text" placeholder="Enter Question Here..."/>
-          <input className="answer correct" type="text" placeholder="Enter CORRECT Answer Here..."/>
-          <input className="answer" type="text" placeholder="Enter Answer Here..."/>
-          <input className="answer" type="text" placeholder="Enter Answer Here..."/>
-          <input className="answer" type="text" placeholder="Enter Answer Here..."/>
-        </form>
+        <Route exact path="/" render={() => <Form getData={this.getData} />} />
+        <Route path='/question' render={() => <Question question={this.state.question} />}/>
       </div>
     );
   }
