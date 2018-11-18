0319c6eb1cc5c2dc6edc1819c0b55dc40af1d502
veselinn16 <veselinn16@gmail.com>
2018-11-18 19:06:53 +0200

    Fix return statement in render method :bug:

--- src/Question.js
+++ src/Question.js
@@ -3,33 +3,39 @@ import React, { Component } from 'react';
 class Question extends Component {
     render() {
         const {question} = this.props // unpack question from props object
-        return(
-            <div className="container">
-                {/* <div className="question">
-                    <h1 className="question">{question.question}</h1>
-                    {question.answersArray ? question.answersArray.map((answer, i) => {
-                        return (
-                        <label>
-                            <input className="ans" type="checkbox" key={i} value={answer}/>
-                        {answer}</label>
-                        )
-                    }) : <h1>Please Enter a Question!</h1>}
-                </div> */}
-                <div className="question-container">
-                    <h1 className="question">Which year was Bulgaria established?</h1>
-                    <div className="answers">
-                        {[682, 681, 732, 1362].map((answer, i) => {
-                            return (
-                            <label>
-                                <input className="ans" type="radio" key={i} name="answer"/>
-                            {answer}</label>
-                            )
-                        })}
-                    </div>
-                    <button className="answer-btn">Submit Answer</button>
+        return(                
+            question.answersArray ?
+            <div className="question-container"> 
+                <h1 className="question">{question.question}</h1>
+                <div className="answers">
+                    {question.answersArray.map((answer, i) => {
+                    return (
+                    <label>
+                        <input className="ans" type="radio" key={i} name="answer"/>
+                    {answer}
+                    </label>
+                    )                
+                })}
                 </div>
-            </div>
+                <button className="answer-btn">Submit Answer</button> 
+            </div> : <h1>Please Enter a Question!</h1>            
         )
+        // return (
+        //  <div className="question-container">
+        //      <h1 className="question">{question.question}</h1>
+        //        <div className="answers">
+        //            {question.answersArray.map((answer, i) => {
+        //                return (
+        //                <label>
+        //                    <input className="ans" type="radio" key={i} name="answer"/>
+        //                {answer}</label>
+        //                )
+        //            })}
+        //        </div>
+        //        <button className="answer-btn">Submit Answer</button>
+        //
+        //    </div>
+        // ) */}
     }
 }
 
