84cb0ca5da0aad676bb3d98d6655dc00c4e3ef3a
veselinn16 <veselinn16@gmail.com>
2018-12-01 23:07:09 +0200

    Add refs to JSX for animations :wrench:

--- src/Arrange.js
+++ src/Arrange.js
@@ -3,108 +3,217 @@ import TweenLite from "gsap/TweenLite";
 import Draggable from 'gsap/Draggable';
 
 class Arrange extends Component {
+    constructor(props) {
+        super(props);
+        this.container = React.createRef();
+        this.listQuestions = []; // Array of question elements
+    }
+
     componentDidMount() {
-        var rowSize   = 100; // => container height / number of items
-        var container = document.querySelector('.container-arrange');
-        var listItems = Array.from(document.querySelectorAll(".list-item")); // Array of elements
-        var sortables = listItems.map(Sortable); // Array of sortables
-        var total     = sortables.length;
+        if(this.listQuestions.length > 0 ) {
+        let rowSize = 100; // => determines the height of a row. Used in layout() function
+        let sortables = this.listQuestions.map(Sortable); // Array of sortables
+        let total = sortables.length; // length of questions
+        const container = this.container; // container ref
 
         TweenLite.to(container, 0.5, { autoAlpha: 1 });
 
+        // const changeIndexL = changeIndex.bind(this);
+
         function changeIndex(item, to) {
             
-        // Change position in array
-        arrayMove(sortables, item.index, to);
-            
-        // Change element's position in DOM. Not always necessary. Just showing how.
-        if (to === total - 1) {
-            container.appendChild(item.element);    
-        } else {    
-            var i = item.index > to ? to : to + 1;
-            container.insertBefore(item.element, container.children[i]);
-        }    
-            
-        // Set index for each sortable
-        sortables.forEach((sortable, index) => sortable.setIndex(index));
+            // Change position in array
+            arrayMove(sortables, item.index, to);
+
+            // Change element's position in DOM. Not always necessary. Just showing how.
+            if (to === total - 1) {
+                container.appendChild(item.element);    
+            } else {
+                var i = item.index > to ? to : to + 1;
+                container.insertBefore(item.element, container.children[i]);
+            }    
+                
+            // Set index for each sortable
+            sortables.forEach((sortable, index) => sortable.setIndex(index));
         }
 
         function Sortable(element, index) {
             
-        var content = element.querySelector(".item-content");
-        var order   = element.querySelector(".order");
-        
-        var animation = TweenLite.to(content, 0.3, {
-            boxShadow: "rgba(0,0,0,0.2) 0px 16px 32px 0px",
-            force3D: true,
-            scale: 1.1,
-            paused: true
-        });
-        
-        var dragger = new Draggable(element, {        
-            onDragStart: downAction,
-            onRelease: upAction,
-            onDrag: dragAction,
-            cursor: "inherit",    
-            type: "y"
-        });
-        
-        // Public properties and methods
-        var sortable = {
-            dragger:  dragger,
-            element:  element,
-            index:    index,
-            setIndex: setIndex
-        };
-        
-        TweenLite.set(element, { y: index * rowSize });
+            let content = element.querySelector(".item-content");
+            let order   = element.querySelector(".order");
             
-        function setIndex(index) {
+            let animation = TweenLite.to(content, 0.3, {
+                boxShadow: "rgba(0,0,0,0.2) 0px 16px 32px 0px",
+                force3D: true,
+                scale: 1.1,
+                paused: true
+            });
             
-            sortable.index = index;    
-            order.textContent = index + 1;
+            let dragger = new Draggable(element, {        
+                onDragStart: downAction,
+                onRelease: upAction,
+                onDrag: dragAction,
+                cursor: "inherit",    
+                type: "y"
+            });
             
-            // Don't layout if you're dragging
-            if (!dragger.isDragging) layout();
-        }
-        
-        function downAction() {
-            animation.play();
-            this.update();
-        }
-        
-        function dragAction() {
+            // Public properties and methods
+            let sortable = {
+                dragger:  dragger,
+                element:  element,
+                index:    index,
+                setIndex: setIndex
+            };
             
-            // Calculate the current index based on element's position
-            var index = clamp(Math.round(this.y / rowSize), 0, total - 1);
-            
-            if (index !== sortable.index) {
-            changeIndex(sortable, index);
+            TweenLite.set(element, { y: index * rowSize });
+                
+            function setIndex(index) {
+                
+                sortable.index = index;    
+                order.textContent = index + 1;
+                
+                // Don't layout if you're dragging
+                if (!dragger.isDragging) layout();
             }
-        }
-        
-        function upAction() {
-            animation.reverse();
-            layout();
-        }
-        
-        function layout() {    
-            TweenLite.to(element, 0.3, { y: sortable.index * rowSize });  
-        }
             
-        return sortable;
+            function downAction() {
+                animation.play();
+                this.update();
+            }
+            
+            function dragAction() {
+                
+                // Calculate the current index based on element's position
+                let index = clamp(Math.round(this.y / rowSize), 0, total - 1);
+                
+                if (index !== sortable.index) {
+                changeIndex(sortable, index);
+                }
+            }
+            
+            function upAction() {
+                animation.reverse();
+                layout();
+            }
+            
+            function layout() {    
+                TweenLite.to(element, 0.3, { y: sortable.index * rowSize });  
+            }
+                
+            return sortable;
         }
 
         // Changes an elements's position in array
         function arrayMove(array, from, to) {
-        array.splice(to, 0, array.splice(from, 1)[0]);
+            array.splice(to, 0, array.splice(from, 1)[0]);
         }
 
         // Clamps a value to a min/max
         function clamp(value, a, b) {
-        return value < a ? a : (value > b ? b : value);
+            return value < a ? a : (value > b ? b : value);
         }
     }
+    }
+
+    // changeIndex(item, to) {
+            
+    //     // Change position in array
+    //     this.arrayMove(this.sortables, item.index, to);
+    //     // Change element's position in DOM. Not always necessary. Just showing how.
+    //     if (to === this.total - 1) {
+    //         this.container.appendChild(item.element);    
+    //     } else {
+    //         var i = item.index > to ? to : to + 1;
+    //         this.container.current.insertBefore(item.element, this.container.current.children[i]);
+    //     }    
+            
+    //     // Set index for each sortable
+    //     this.sortables.forEach((sortable, index) => sortable.setIndex(index));
+    // }
+
+    // Sortable(element, index) {
+            
+    //     let content = element.querySelector(".item-content");
+    //     let order   = element.querySelector(".order");
+        
+    //     let animation = TweenLite.to(content, 0.3, {
+    //         boxShadow: "rgba(0,0,0,0.2) 0px 16px 32px 0px",
+    //         force3D: true,
+    //         scale: 1.1,
+    //         paused: true
+    //     });
+        
+    //     let dragger = new Draggable(element, {        
+    //         onDragStart: this.downAction,
+    //         onRelease: this.upAction,
+    //         onDrag: this.dragAction,
+    //         cursor: "inherit",    
+    //         type: "y"
+    //     });
+        
+    //     // Public properties and methods
+    //     let sortable = {
+    //         dragger:  dragger,
+    //         element:  element,
+    //         index:    index,
+    //         setIndex: this.setIndex
+    //     };
+        
+    //     TweenLite.set(element, { y: index * this.rowSize });
+
+    //     this.setIndex(index, order, dragger, sortable, element);
+
+    //     this.downAction(animation);
+
+    //     this.dragAction(sortable);
+
+    //     this.upAction(animation);
+
+    //     this.layout(element, sortable);
+            
+    //     return sortable;
+    // }
+
+    // setIndex(index, order, dragger, sortable, element) {            
+    //     sortable.index = index;    
+    //     order.textContent = index + 1;
+        
+    //     // Don't layout if you're dragging
+    //     (!dragger.isDragging) && this.layout(element, sortable);
+    // }
+
+    // downAction(animation) {
+    //     animation.play();
+    //     this.update();
+    // }
+
+    // dragAction(sortable) {
+            
+    //     // Calculate the current index based on element's position
+    //     let index = this.clamp(Math.round(this.y / this.rowSize), 0, this.total - 1);
+        
+    //     if (index !== sortable.index) {
+    //         this.changeIndex(sortable, index);
+    //     }
+    // }
+
+    // upAction(animation) {
+    //     animation.reverse();
+    //     this.layout();
+    // }
+
+    // layout(element, sortable) {    
+    //     TweenLite.to(element, 0.3, { y: sortable.index * this.rowSize });  
+    // }
+
+    // arrayMove(array, from, to) {
+    //     array.splice(to, 0, array.splice(from, 1)[0]);
+    // }
+
+    // clamp(value, a, b) {
+    //     return value < a ? a : (value > b ? b : value);
+    // }
+
     render() {
         return (
             // this.props.questions.length > 0 ?
@@ -116,16 +225,15 @@ class Arrange extends Component {
             //     </ol>                
             // </div> : <h1>Enter questions please!</h1>
             this.props.questions.length > 0 ?
-            <section className="container-arrange">
+            <div className="container-arrange" ref={container => this.container = container}>
                 {this.props.questions.map((question, i) => {
-                return <div className="list-item">
+                return <div className="list-item" key={i} ref={question => this.listQuestions[i] = question}>
                             <div className="item-content">
-                                <span key={i} className='order'>{i + 1}</span> {question.question}
+                                <span className='order'>{i + 1}</span> {question.question}
                             </div>
                         </div>})}
-            </section>            
+            </div>            
             : <h1>Enter questions please!</h1>
-            
         )
     }
 }
