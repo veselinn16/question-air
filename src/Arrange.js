b66919b4707b253607ac3807a534558bc6a1da8e
veselinn16 <veselinn16@gmail.com>
2018-12-03 00:14:24 +0200

    Comment out code :books:

--- src/Arrange.js
+++ src/Arrange.js
@@ -11,26 +11,27 @@ class Arrange extends Component {
 
     componentDidMount() {
         if(this.listQuestions.length > 0 ) {
-        let rowSize = 100; // => determines the height of a row. Used in layout() function
-        let sortables = this.listQuestions.map(Sortable); // Array of sortables
-        let total = sortables.length; // length of questions
+        const rowSize = 100; // => determines the height of a row. Used in layout() function
+        const sortables = this.listQuestions.map(Sortable); // array of questions
+        let total = sortables.length; // length of questions array
         const container = this.container; // container ref
+        console.log('1)variables are defined')
 
-        TweenLite.to(container, 0.5, { autoAlpha: 1 });
-
+        TweenLite.to(container, 0.5, { autoAlpha: 1 }); //creates a TweenLite instance, which animates the container to the specified values from the current ones
+        console.log('2) Tween is set');
         // const changeIndexL = changeIndex.bind(this);
 
         function changeIndex(item, to) {
-            
+            console.log('(1) changeIndex() is running.');
             // Change position in array
             arrayMove(sortables, item.index, to);
 
             // Change element's position in DOM. Not always necessary. Just showing how.
             if (to === total - 1) {
-                container.appendChild(item.element);    
+                container.appendChild(item.element); // if the question's index is equal to the total length of the questions array append the question to the container at the bottom
             } else {
-                var i = item.index > to ? to : to + 1;
-                container.insertBefore(item.element, container.children[i]);
+                let i = item.index > to ? to : to + 1; // sets i based on whether the question's index is bigger than the old index or lower
+                container.insertBefore(item.element, container.children[i]); // inserts the question before the index of the question which has to be after it
             }    
                 
             // Set index for each sortable
@@ -38,15 +39,15 @@ class Arrange extends Component {
         }
 
         function Sortable(element, index) {
-            
+            console.log('(2) Sortable() is running.');
             let content = element.querySelector(".item-content");
             let order   = element.querySelector(".order");
             
             let animation = TweenLite.to(content, 0.3, {
                 boxShadow: "rgba(0,0,0,0.2) 0px 16px 32px 0px",
-                force3D: true,
+                force3D: true, // keeps element in 3D mode.  PLAY AROUND WITH THIS!!
                 scale: 1.1,
-                paused: true
+                paused: true // animation pauses itself immediately after creation
             });
             
             let dragger = new Draggable(element, {        
@@ -57,60 +58,69 @@ class Arrange extends Component {
                 type: "y"
             });
             
-            // Public properties and methods
+            // Public properties and methods.
             let sortable = {
-                dragger:  dragger,
-                element:  element,
-                index:    index,
-                setIndex: setIndex
+                dragger,
+                element,
+                index,
+                setIndex
             };
             
-            TweenLite.set(element, { y: index * rowSize });
+            TweenLite.set(element, { y: index * rowSize }); // sets the position of the item based on its index. F.ex.: for the first question el is 0 * 100 = 0
                 
             function setIndex(index) {
+                console.log('(3) setIndex() is running.');
+                sortable.index = index; // sets the new index of the question  
+                order.textContent = index + 1; // sets the text of the order element based on index of question
                 
-                sortable.index = index;    
-                order.textContent = index + 1;
-                
-                // Don't layout if you're dragging
-                if (!dragger.isDragging) layout();
+                (!dragger.isDragging) && layout(); // don't layout if you're dragging
             }
             
             function downAction() {
-                animation.play();
-                this.update();
+                // Called as soon as mouse moves 2px(dragging has begun)
+                console.log('(4) downAction() is running.');
+                animation.play(); // animates box-shadow, scale, translate3d(), paused
+                this.update(); // updates the question(Draggable instance)'s y property to reflect its modified position
             }
             
             function dragAction() {
-                
+                // called every single time the mouse moves during drag
+                console.log('(5) dragAction() is running.');
                 // Calculate the current index based on element's position
-                let index = clamp(Math.round(this.y / rowSize), 0, total - 1);
+                let index = clamp(Math.round(this.y / rowSize), 0, total - 1); // returns the index where the element should be placed among the order of questions
                 
-                if (index !== sortable.index) {
-                changeIndex(sortable, index);
+                if (index !== sortable.index) { // if index is modified e.g. element is moved
+                    changeIndex(sortable, index); // change its index
                 }
             }
             
             function upAction() {
+                // called when the dragging ends
+                console.log('(6) upAction() is running.');
                 animation.reverse();
                 layout();
             }
             
-            function layout() {    
+            function layout() {
+                // creates a TweenLite instance, which animates the question element's specified values - y
+                console.log('(7) layout() is running.');    
                 TweenLite.to(element, 0.3, { y: sortable.index * rowSize });  
             }
                 
             return sortable;
         }
 
-        // Changes an elements's position in array
         function arrayMove(array, from, to) {
+            // changes an elements's position in array
+            console.log('(8) outside of Sortable arrayMove() is running.');    
             array.splice(to, 0, array.splice(from, 1)[0]);
         }
 
-        // Clamps a value to a min/max
         function clamp(value, a, b) {
-            return value < a ? a : (value > b ? b : value);
+            // clamps a value to a min/max
+            // maybe clamp is not the best name, place is better
+            console.log('(9) outside of Sortable() clamp() is running.');    
+            return value < a ? a : (value > b ? b : value); // value is the row, based on the location of the question element, and compares it with 0(first row) and value(the final row)
         }
     }
     }
