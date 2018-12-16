6b68be27d349230fc8b9724332d409c95b18f781
veselinn16 <veselinn16@gmail.com>
2018-12-16 23:49:27 +0200

    Reveal result and correct answers of questions :sparkles:

--- src/Results.js
+++ src/Results.js
@@ -1,10 +1,21 @@
 import React , { Component } from 'react';
 
 class Results extends Component {
+    componentDidMount() {
+        console.log(this.props.questions);
+    }
     render() {
         return (
             <div>
                 <p>Your score is {this.props.score}</p>
+                {this.props.questions.map(el => {
+                    return(
+                    <div>
+                        <p>Question: {el.question}</p>
+                        <p>Your response was: {el.response}</p>
+                        <p>The correct answer was: {el.answer}</p>
+                    </div>)
+                })}
             </div>
             )
     }
