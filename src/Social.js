6d8723e136729fee8ddd2b11a90736d8236b0b3d
veselinn16 <veselinn16@gmail.com>
2019-01-17 13:30:53 +0200

    Make icons reusable :wrench:

--- src/Social.js
+++ src/Social.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import Icon from './Icon';
 
-const Social = () => {
+const Social = (props) => {
     return (
         <footer className="footer">
-            <a href="https://www.instagram.com/vesel.out/" rel="nopener noreferrer" target="_blank">
-                <Icon dims='32' type='instagram' class="instagram"/>
+            <a href="https://www.instagram.com/vesel.out/" rel="noopener noreferrer" target="_blank">
+                <Icon dims='32' type='instagram' class={`instagram instagram-${props.inst}`}/>
             </a>
-            <a href="https://www.facebook.com/profile.php?id=100000450630612" target="_blank">
-                <Icon dims='32' type='facebook2' class="facebook"/>
+            <a href="https://www.facebook.com/profile.php?id=100000450630612" rel="noopener noreferrer" target="_blank">
+                <Icon dims='32' type='facebook2' class={`facebook facebook-${props.inst}`}/>
             </a>
-            <a href="https://github.com/veselinn16" target="_blank">
-                <Icon dims='32' type='github' class="github"/>
+            <a href="https://github.com/veselinn16" rel="noopener noreferrer" target="_blank">
+                <Icon dims='32' type='github' class={`github github-${props.inst}`}/>
             </a>
-            <a href="https://www.linkedin.com/in/veselin-tonev-173b09152/" rel="nopener noreferrer" target="_blank">
+            <a href="https://www.linkedin.com/in/veselin-tonev-173b09152/" rel="noopener noreferrer" target="_blank" class={`github github-${props.inst}`}>
                 <Icon dims='32' type='linkedin' class="linkedin"/>
             </a>
             <p className="copyright">&copy; Veselin Tonev 2019</p>
