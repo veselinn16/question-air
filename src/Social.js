bedba163391edbe9cf65b2534047f469d50e8080
veselinn16 <veselinn16@gmail.com>
2019-01-24 11:25:48 +0200

    Add classes via props to social component :wrench:

--- src/Social.js
+++ src/Social.js
@@ -3,18 +3,18 @@ import Icon from './Icon';
 
 const Social = (props) => {
     return (
-        <footer className="footer">
+        <footer className={props.class}>
             <a href="https://www.instagram.com/vesel.out/" rel="noopener noreferrer" target="_blank">
-                <Icon dims='32' type='instagram' class={`instagram instagram-${props.inst}`}/>
+                <Icon dims='32' type='instagram' classes={`instagram instagram-${props.inst}`}/>
             </a>
             <a href="https://www.facebook.com/profile.php?id=100000450630612" rel="noopener noreferrer" target="_blank">
-                <Icon dims='32' type='facebook2' class={`facebook facebook-${props.inst}`}/>
+                <Icon dims='32' type='facebook2' classes={`facebook facebook-${props.inst}`}/>
             </a>
             <a href="https://github.com/veselinn16" rel="noopener noreferrer" target="_blank">
-                <Icon dims='32' type='github' class={`github github-${props.inst}`}/>
+                <Icon dims='32' type='github' classes={`github github-${props.inst}`}/>
             </a>
-            <a href="https://www.linkedin.com/in/veselin-tonev-173b09152/" rel="noopener noreferrer" target="_blank" class={`github github-${props.inst}`}>
-                <Icon dims='32' type='linkedin' class="linkedin"/>
+            <a href="https://www.linkedin.com/in/veselin-tonev-173b09152/" rel="noopener noreferrer" target="_blank" classes={`linkedin linkedin-${props.inst}`}>
+                <Icon dims='32' type='linkedin' classes={`github github-${props.inst}`}/>
             </a>
             <p className="copyright">&copy; Veselin Tonev 2019</p>
         </footer>
