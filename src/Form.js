91a9d2fba4b4cb7d3ccd4ce036a5ed57b8146338
veselinn16 <veselinn16@gmail.com>
2019-01-24 11:28:51 +0200

    Use new social component in all routes :wrench:

--- src/Form.js
+++ src/Form.js
@@ -8,19 +8,19 @@ class Form extends Component {
         const {hideWarning, getData} = this.props // props object destructuring
         return(
         <div className="index" ref="index">
-            <Icon dims='640' type='logo' class="logo"/>
+            <Icon dims='640' type='logo' classes="logo logo-1"/>
             <form className="form" onChange={hideWarning}>
                 <h2 className="title">Question Form</h2>
-                <input className="question" type="text" placeholder="Enter Question Here..."/>
-                <input className="answer correct" type="text" placeholder="Enter CORRECT Answer Here..."/>
-                <input className="answer" type="text" placeholder="Enter Answer Here..."/>
-                <input className="answer" type="text" placeholder="Enter Answer Here..."/>
-                <input className="answer" type="text" placeholder="Enter Answer Here..."/>
+                <input className="question" type="text" spellCheck="false" placeholder="Enter Question Here..."/>
+                <input className="answer correct" type="text" spellCheck="false" placeholder="Enter CORRECT Answer Here..."/>
+                <input className="answer" type="text" spellCheck="false" placeholder="Enter Answer Here..."/>
+                <input className="answer" type="text" spellCheck="false" placeholder="Enter Answer Here..."/>
+                <input className="answer" type="text" spellCheck="false" placeholder="Enter Answer Here..."/>
                 <button className="submit" onClick={getData}>Submit</button>
             </form>
             <Link className="btn btn-question" to='/question-1'>Show Question</Link>
             <Link className="btn btn-arrange" to='/arrange-questions'>Arrange Questions</Link>
-            <Social/>
+            <Social inst="1" class="footer-1"/>
         </div>
         )
     }
