7a4d6626d487c2f2896872dcaa12decf3e308be3
veselinn16 <veselinn16@gmail.com>
2019-01-26 22:49:11 +0200

    Add functionality for results button :wrench:

---
+++ src/resultsBtn.js
@@ -0,0 +1,44 @@
+import { TimelineLite, Elastic, Power3, SlowMo } from 'gsap';
+
+function getRandom(min, max){
+  return Math.random() * (max - min) + min;
+}
+
+export default function initBt2(button) {
+  var bt = button;
+  var filter = document.querySelectorAll('#filter-goo-2 feGaussianBlur')[0];
+  var particleCount = 12;
+  var colors = ['#DE8AA0', '#8AAEDE', '#FFB300', '#60C7DA']
+
+  bt.addEventListener('click', function() {
+    var particles = [];
+    var tl = new TimelineLite({onUpdate: function() {
+      filter.setAttribute('x', 0);
+    }});
+    
+    tl.to(bt.querySelectorAll('.button__bg'), 0.6, { scaleX: 1.05 });
+    tl.to(bt.querySelectorAll('.button__bg'), 0.9, { scale: 1, ease: Elastic.easeOut.config(1.2, 0.4) }, 0.6);
+
+    for (var i = 0; i < particleCount; i++) {
+      particles.push(document.createElement('span'));
+      bt.appendChild(particles[i]);
+
+      particles[i].classList.add(i % 2 ? 'lefty' : 'righty');
+      
+      var dir = i % 2 ? '-' : '+';
+      var r = i % 2 ? getRandom(-1, 1)*i/2 : getRandom(-1, 1)*i;
+      var size = i < 2 ? 1 : getRandom(0.4, 0.8);
+      var tl = new TimelineLite({ onComplete: function(i) {
+        particles[i].parentNode.removeChild(particles[i]);
+        this.kill();
+      }, onCompleteParams: [i] });
+
+      tl.set(particles[i], { scale: size });
+      tl.to(particles[i], 0.6, { x: dir + 20, scaleX: 3, ease: SlowMo.ease.config(0.1, 0.7, false) });
+      tl.to(particles[i], 0.1, { scale: size, x: dir +'=25' }, '-=0.1');
+      if(i >= 2) tl.set(particles[i], { backgroundColor: colors[Math.round(getRandom(0, 3))] });
+      tl.to(particles[i], 0.6, { x: dir + getRandom(60, 100), y: r*10, scale: 0.1, ease: Power3.easeOut });
+      tl.to(particles[i], 0.2, { opacity: 0, ease: Power3.easeOut }, '-=0.2');
+    }
+  });
+}
