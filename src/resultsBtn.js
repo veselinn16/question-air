5f34347b3c3d7fdca0288bfd8eee4884f4872dd1
veselinn16 <veselinn16@gmail.com>
2019-01-29 21:12:59 +0200

    Fix commonly named variables :bug:

--- src/resultsBtn.js
+++ src/resultsBtn.js
@@ -5,40 +5,40 @@ function getRandom(min, max){
 }
 
 export default function initBt2(button) {
-  var bt = button;
-  var filter = document.querySelectorAll('#filter-goo-2 feGaussianBlur')[0];
-  var particleCount = 12;
-  var colors = ['#DE8AA0', '#8AAEDE', '#FFB300', '#60C7DA']
+  let bt = button;
+  let filter = document.querySelectorAll('#filter-goo feGaussianBlur')[0];
+  let particleCount = 12;
+  let colors = ['#DE8AA0', '#8AAEDE', '#FFB300', '#60C7DA']
 
   bt.addEventListener('click', function() {
-    var particles = [];
-    var tl = new TimelineLite({onUpdate: function() {
+    let particles = [];
+    let tl = new TimelineLite({onUpdate: function() {
       filter.setAttribute('x', 0);
     }});
     
-    tl.to(bt.querySelectorAll('.button__bg'), 0.6, { scaleX: 1.05 });
-    tl.to(bt.querySelectorAll('.button__bg'), 0.9, { scale: 1, ease: Elastic.easeOut.config(1.2, 0.4) }, 0.6);
+    tl.to(bt.querySelectorAll('.btn-bg'), 0.6, { scaleX: 1.05 });
+    tl.to(bt.querySelectorAll('.btn-bg'), 0.9, { scale: 1, ease: Elastic.easeOut.config(1.2, 0.4) }, 0.6);
 
-    for (var i = 0; i < particleCount; i++) {
+    for (let i = 0; i < particleCount; i++) {
       particles.push(document.createElement('span'));
       bt.appendChild(particles[i]);
 
       particles[i].classList.add(i % 2 ? 'lefty' : 'righty');
       
-      var dir = i % 2 ? '-' : '+';
-      var r = i % 2 ? getRandom(-1, 1)*i/2 : getRandom(-1, 1)*i;
-      var size = i < 2 ? 1 : getRandom(0.4, 0.8);
-      var tl = new TimelineLite({ onComplete: function(i) {
+      let dir = i % 2 ? '-' : '+';
+      let r = i % 2 ? getRandom(-1, 1)*i/2 : getRandom(-1, 1)*i;
+      let size = i < 2 ? 1 : getRandom(0.4, 0.8);
+      let tl1 = new TimelineLite({ onComplete: function(i) {
         particles[i].parentNode.removeChild(particles[i]);
         this.kill();
       }, onCompleteParams: [i] });
 
-      tl.set(particles[i], { scale: size });
-      tl.to(particles[i], 0.6, { x: dir + 20, scaleX: 3, ease: SlowMo.ease.config(0.1, 0.7, false) });
-      tl.to(particles[i], 0.1, { scale: size, x: dir +'=25' }, '-=0.1');
-      if(i >= 2) tl.set(particles[i], { backgroundColor: colors[Math.round(getRandom(0, 3))] });
-      tl.to(particles[i], 0.6, { x: dir + getRandom(60, 100), y: r*10, scale: 0.1, ease: Power3.easeOut });
-      tl.to(particles[i], 0.2, { opacity: 0, ease: Power3.easeOut }, '-=0.2');
+      tl1.set(particles[i], { scale: size });
+      tl1.to(particles[i], 0.6, { x: dir + 20, scaleX: 3, ease: SlowMo.ease.config(0.1, 0.7, false) });
+      tl1.to(particles[i], 0.1, { scale: size, x: dir +'=25' }, '-=0.1');
+      if(i >= 2) tl1.set(particles[i], { backgroundColor: colors[Math.round(getRandom(0, 3))] });
+      tl1.to(particles[i], 0.6, { x: dir + getRandom(60, 100), y: r*10, scale: 0.1, ease: Power3.easeOut });
+      tl1.to(particles[i], 0.2, { opacity: 0, ease: Power3.easeOut }, '-=0.2');
     }
   });
 }
