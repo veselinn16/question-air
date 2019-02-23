850a00bfa6e93c478bbf4c37c0390d4dc4b3bcda
veselinn16 <veselinn16@gmail.com>
2019-02-23 18:07:13 +0200

    Format Error component :art:

--- src/routes/Error.js
+++ src/routes/Error.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// import { f } from '../undraw_questions_75e0.svg';
+
+
+//  console.log(f);
 
 const Error = ({text, recommendation}) => {
     return(
         <div className="error-container">
             <div className="content-container">
-                <div className="error-illustration">
-                
+                <div className="error-illustration-container">
+                    <img className="error-illustration" src="../undraw_questions_75e0.svg" alt="No Questions Illustration"/>
                 </div>
-                <div className="error-heading">
-                    <h1>{text}</h1>
-                </div>
-                <div className="error-recommendation">
-                    <p>{recommendation}</p>
+                <div className="error-text-container">
+                    <h1 className="error-heading">{text}</h1>
+                    <p className="error-recommendation">{recommendation}</p>
+                    <Link to='/' className="error-link">Back home <span className="arrow">&rarr;</span></Link>
                 </div>
             </div>
-            <Link to='/' className="error-link">Back home</Link>
         </div>
     )
 }
