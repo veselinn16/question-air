f3673309097297fb895696a0d03bf47ac099e2f5
veselinn16 <veselinn16@gmail.com>
2019-02-24 22:48:04 +0200

    Add 404 component :construction:

---
+++ src/routes/FourOhFour.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const FourOhFour = () => {
+    return(
+        <div className="four-oh-four">
+            <div className="distortion-effect">
+                <h1 className="four-oh-four-message">404</h1>    
+            </div>
+            <h1 className="four-oh-four-message">404</h1>
+        </div>
+    )
+}
