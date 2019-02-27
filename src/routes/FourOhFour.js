4e595906369bdf5ad6ba20146ecc97407f837dc9
veselinn16 <veselinn16@gmail.com>
2019-02-27 21:56:49 +0200

    Restructure button architecture :construction:

--- src/routes/FourOhFour.js
+++ src/routes/FourOhFour.js
@@ -6,7 +6,7 @@ export const FourOhFour = () => {
         <div className="four-oh-four-container">
             <h1 className="four-oh-four-status">404</h1>
             <h2 className="four-oh-four-message">The page you requested is not yet built by the developer!</h2>
-            <Link to="/" className="link-404">Go Home</Link>
+            <Link to="/" className="link-404"><span className="link-404-text">Go Home</span></Link>
         </div>
     )
 }
