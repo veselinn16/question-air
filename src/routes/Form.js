2da66a2c5889021eb67fe3387ce53ab5710b9d1e
veselinn16 <veselinn16@gmail.com>
2019-03-28 23:25:03 +0200

    Move event handler from button to form element :recycle:

--- src/routes/Form.js
+++ src/routes/Form.js
@@ -6,14 +6,14 @@ import Social from '../components/Social';
 const Form =({hideWarning, getData}) => (
     <div className="form-container">
         <Icon dims='640' type='logo' classes="logo logo-1"/>
-        <form className="form" onChange={hideWarning}>
+        <form className="form" onChange={hideWarning} onSubmit={getData}>
             <h2 className="title">Question Form</h2>
             <input className="question" type="text" spellCheck="false" placeholder="Enter Question Here..."/>
             <input className="answer correct" type="text" spellCheck="false" placeholder="Enter CORRECT Answer Here..."/>
             <input className="answer" type="text" spellCheck="false" placeholder="Enter Answer Here..."/>
             <input className="answer" type="text" spellCheck="false" placeholder="Enter Answer Here..."/>
             <input className="answer" type="text" spellCheck="false" placeholder="Enter Answer Here..."/>
-            <button className="submit" onClick={getData}>Submit</button>
+            <button className="submit">Submit</button>
         </form>
         <Link className="btn btn-question" to='/question'>Show Questions</Link>
         <Link className="btn btn-arrange" to='/arrange-questions'>Arrange Questions</Link>
